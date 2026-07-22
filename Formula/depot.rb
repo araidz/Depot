a1b1aaabd97623753a176e973fe8594976fbee78
araidz <riadm.ahmed@gmail.com>
2026-07-22 15:30:29 +0400

    Formula: v0.1.1 tarball sha256

--- Formula/depot.rb
+++ Formula/depot.rb
@@ -2,7 +2,7 @@ class Depot < Formula
   desc "Lightweight macOS installer & firmware downloader TUI"
   homepage "https://github.com/araidz/Depot"
   url "https://github.com/araidz/Depot/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "PLACEHOLDER"
+  sha256 "0d66f7a82adb73657f0513c3884c652f6506d454053916fbc6cc43f16b0bee58"
   license "MIT"
 
   depends_on "aria2"
